1fdc999d14f9a22fabcfa3b2409bf7a1dbf228db
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-10-14 20:22:08 +0800

    Update usage.js

--- src/commands/core/usage.js
+++ src/commands/core/usage.js
@@ -1,5 +1,4 @@
 const { EmbedBuilder } = require("discord.js")
-const SI = require("systeminformation")
 const os = require("os-utils")
 const config = require("../../../config.json")
 module.exports = {
@@ -14,7 +13,7 @@ module.exports = {
 			arr.reverse()
 			const used = process.memoryUsage().heapUsed / 1024 /1024
 			let memory = `Approximate memory usage: ${Math.round(used * 100) / 100} MB`
-			let CPU = `CPU Usage: ${Math.round(v * 100) / 100}%\n Temperature: ${SI.cpuTemperature}`
+			let CPU = `CPU Usage: ${Math.round(v * 100) / 100}%`
 			const embed = new EmbedBuilder()
 			embed.setColor(config.defaultembedcolour)
 			embed.setTitle("Jasbot's Usage")
