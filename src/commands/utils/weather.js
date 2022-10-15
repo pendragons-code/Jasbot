edd4fa00f724c4c84c1b3120350fbd08a484a038
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-10-15 17:04:23 +0800

    Add files via upload

--- src/commands/utils/weather.js
+++ src/commands/utils/weather.js
@@ -17,13 +17,13 @@ module.exports = {
 	  	embed.setColor(defaultembedcolour)
 		embed.setFooter({ text: defaultfootertext })
 	  	embed.setDescription("Temperature units can may be differ some time")
-	  	embed.addField("Temperature", `${result[0].current.temperature} Celsius`, true)
-	  	embed.addField("Temperature", `${p} Fahrenheit`, true)
-	  	embed.addField("Sky Text", result[0].current.skytext, true)
-	  	embed.addField("Humidity", result[0].current.humidity, true)
-	  	embed.addField("Wind Speed", result[0].current.windspeed, true)//What about image
-	  	embed.addField("Observation Time", result[0].current.observationtime, true)
-	  	embed.addField("Wind Display", result[0].current.winddisplay, true)
+	  	embed.addFields({ name: "Temperature", value: `${result[0].current.temperature} Celsius`})
+	  	embed.addFields({ name: "Temperature", value: `${p} Fahrenheit`})
+	  	embed.addFields({ name: "Sky Text", value: result[0].current.skytext})
+	  	embed.addFields({ name: "Humidity", value: result[0].current.humidity})
+	  	embed.addFields({ name: "Wind Speed", value: result[0].current.windspeed})//What about image
+	  	embed.addFields({ name: "Observation Time", value: result[0].current.observationtime})
+	  	embed.addFields({ name: "Wind Display", value: result[0].current.winddisplay})
 	  	embed.setThumbnail(result[0].current.imageUrl);
 	  	messageCreate.channel.send({embeds:[embed]}).catch(() =>{
 			return messageCreate.channel.send(reject.ExecutionError)
