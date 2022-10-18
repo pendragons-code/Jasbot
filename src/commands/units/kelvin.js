eed38ac0eda269b872a70b2eaa2a7f16c799103a
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-10-19 03:01:27 +0800

    Add files via upload

--- src/commands/units/kelvin.js
+++ src/commands/units/kelvin.js
@@ -1,5 +1,28 @@
+const { EmbedBuilder } = require("discord.js")
+const { defaultembedcolour, defaultfootertext } = require("../../../config.json")
+const reject = require("../../../assets/items/rejection.json")
 module.exports = {
 	name: "kelvin",
 	aliases: [],
-	category: "units"
+	category: "units",
+	desc: "Converting kelvin to some other common units of temperature.",
+	utilisation: "kelvin <temperature in fahrenheit>",
+	async execute(bot, messageCreate, args, prefix){
+		if(!args[0]) return messageCreate.channel.send(reject.user.numbers.missing)
+		if(args[1]) return messageCreate.channel.send(reject.user.args.toomany)
+		if(isNaN(parseFloat(args[0]))) return messageCreate.channel.send(reject.user.numbers.invalid)
+		let fahrenheit = (parseFloat(args[0]) - 273.15)
+		let celsius = (parseFloat(args[0]) - 273.15) * 1.8 + 32
+		let desc = `${args[0]} kelvin(s) is equal to:\n${fahrenheit} fahrenheit \n${celsius} celsius`
+		const embed = new EmbedBuilder()
+		embed.setTitle("Conversion of units of temperture!")
+		embed.setColor(defaultembedcolour)
+		embed.setFooter({ text: defaultfootertext })
+		embed.setDescription(desc)
+		embed.setTimestamp()
+		messageCreate.channel.send({ embeds: [embed] }).catch(() => {
+			console.err()
+			return messageCreate.channel.send(reject.ExecutionError)
+		})
+	}
 }
