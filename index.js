4bd92e8e2b01af05aceca1289ed422522b53723f
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-10-27 01:13:51 +0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const env = require("dotenv").config()
-const { eventer, structurer, commander, bot } = require("./bot.js")
-console.log("A terribly made discord bot.")
-eventer()
-structurer()
-commander()
-bot.login(process.env.token)
-process.traceDeprecation = true
