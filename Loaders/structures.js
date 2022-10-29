5ac339d63719d2dc227ff641cd03d9d8d27e403c
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-10-30 02:02:29 +0800

    Update structures.js
    mother fu-

--- Loaders/structures.js
+++ Loaders/structures.js
@@ -8,7 +8,7 @@ function structurer(){
 		for(const file of structure){
 			const structure = require(`../src/structure/${dirs}/${file}`)
 			console.log(`Loading structure component: ${file} from ${dirs}`)
-			bot.structure.set(structure.name.toLowerCase(), structure)
+			bot.structures.set(structure.name.toLowerCase(), structure)
 		}
 	})
 	console.log(`${decorpiece} End of Stuctural components ${decorpiece2}`)
