212bbc7ee489fd6eb1500164e74170374b3eee2b
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-11-01 00:11:12 +0800

    Add files via upload

--- src/structure/user/antiswear.js
+++ src/structure/user/antiswear.js
@@ -4,20 +4,19 @@ const { guildconfigdb } = require("../../../bot.js")
 module.exports = {
 	name: "antiswear",
 	async execute(bot, messageCreate){
-		console.log(messageCreate.content)
 		let swearconfig = await guildconfigdb.get(`swearconfig_${messageCreate.guild.id}`)
-		if(swearconfig === null) guildconfigdb.set(`swearconfig_${messageCreate.guild.id}`, DefaultSwear)
+		if(swearconfig === null || swearconfig.length === null) await guildconfigdb.set(`swearconfig_${messageCreate.guild.id}`, DefaultSwear)
 		let warnonoff = await guildconfigdb.get(`warnonoff_${messageCreate.guild.id}`)
 		if(warnonoff === null) warnonoff = "off"
 		let cusscheck = await guildconfigdb.get(`cuss_${messageCreate.guild.id}`)
 		if(cusscheck === null) cusscheck === "off"
-		if(cusscheck === "off") return
-		for(i=0;i<swearconfig.length;i++){
+		if(cusscheck === "off" || cusscheck === null) return
+		for(i=0;i<swearconfig.length;i++){ 
 			if(messageCreate.content.toLowerCase().includes(swearconfig[i])){
 				if(warnonoff === "on") warn(messageCreate.author, `Used vulgar word: ${messageCreate.content}`, 'Jasbot-antiswear')
 				messageCreate.channel.send("Anti-swear is on! Please do not swear again!")
 				return messageCreate.delete()
-		}
+			}
 		}
 	}
 }
