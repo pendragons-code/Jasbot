990d2d6c454abf0faada24562eb8b8f915dc225b
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-11-03 22:09:14 +0800

    Add files via upload

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,16 +3,14 @@ module.exports = async (bot) => {
 	const os = require("os-utils")
 	let editmode = await botconfigdb.get("editmode")
 	os.cpuUsage(function(v){
-		const used = process.memoryUsage().heapUsed / 1024 /1024
-        	let memory = `Approximate memory usage: ${Math.round(used * 100) / 100} MB`
+		const used = process.memoryUsage.rss()/ 1024 /1024
+        let memory = `Approximate memory usage: ${Math.round(used * 100) / 100} MB`
 		let Desc = `CPU Usage: ${Math.round(v * 100) / 100}%\n${memory}`
-		console.log(`	Status and uptime: 	\nLogged in as ${bot.user.username}	\n\n\n\nServers:	\nReady on ${bot.guilds.cache.size} servers!	\n\n\n\nUsers:	\nServing ${bot.users.cache.size} users!	\n\n\n\nHardware report!:	\n${Desc}`)
-
+		console.log(`Status and uptime: 	\nLogged in as ${bot.user.username}	\n\n\n\nServers:	\nReady on ${bot.guilds.cache.size} servers!	\n\n\n\nUsers:	\nServing ${bot.users.cache.size} users!	\n\n\n\nHardware report!:	\n${Desc}`)
 		setInterval(function(){ 
 			if(editmode === null || editmode == 1) bot.user.setActivity("Use 'jas help' for commands!")
 			if(editmode == 0) bot.user.setActivity("Editmode is on!")
 		}, 3000)
-
 		})
 	bot.server
 }
