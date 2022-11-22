ba517985665c52d7d0b22c2421e308f4bb167f92
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-11-23 03:38:08 +0800

    Update credit.js

--- src/commands/core/credit.js
+++ src/commands/core/credit.js
@@ -27,8 +27,8 @@ module.exports = {
 		embed.setTitle("Credits!")
 		embed.setDescription(description)
 		embed.setTimestamp()
-		messageCreate.channel.send({ embeds: [embed] }).catch(() => {
-			console.error()
+		messageCreate.channel.send({ embeds: [embed] }).catch((error)=>{
+			console.error("error", error)
 			return messageCreate.channel.send(reject.ExecutionError)
 		})
 	}
