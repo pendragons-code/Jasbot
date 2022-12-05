bb7376b0660097a8a2a9354b6d556741d6b5100c
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-12-05 18:13:34 +0800

    Too lazy to sift through file by file.
    Another update

--- src/functions/RedditRequest.js
+++ src/functions/RedditRequest.js
@@ -1,6 +1,7 @@
 async function RedditRequestSFW(request){
 	const axios = await import("axios")
 	let response = await axios.default(request)
+	if(!response.data[0]) return 404
 	let output = response.data[0].data.children[0].data
 	if(output.over_18){
 		console.log("nsfw, retry")
@@ -11,19 +12,22 @@ async function RedditRequestSFW(request){
 
 async function RedditRequestNSFW(request){
 	const axios = await import("axios")
-	axios.default(request).then(response => {
-		let output = response.data[0].data.children[0].data
-		return output
-	})
+	let response = await axios.default(request)
+	if(!response.data[0]) return 404
+	let output = response.data[0].data.children[0].data
+	return output
 }
 
 async function CheckSubSFW(request){
 	const axios = await import("axios")
-	axios.default(request).then(response => {
-		let output = response.data.data
-		if(output.over18) return "over18"
-		return "safe"
-	})
+	axios.default(request)
+	if(!response.data[0]) return 404
+	let output = response.data.data
+	if(output.over18) return "over18"
+	return "safe"
 }
+
+
+// Note that output.error seems to exist on some other requests that suceed!
 
 module.exports = { RedditRequestSFW, RedditRequestNSFW, CheckSubSFW }
