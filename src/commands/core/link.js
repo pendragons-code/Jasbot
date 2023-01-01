4c846689f397622d53577fafadd6f72916eda0e5
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2023-01-02 01:27:20 +0800

    Update link.js

--- src/commands/core/link.js
+++ src/commands/core/link.js
@@ -3,7 +3,7 @@ module.exports = {
 	aliases: [],
 	category: "core",
 	utilisation: "link <dm/here>",
-	description: "Sends link to add the discord bot! Not specifying where the link should be sent would mean the bot will send the message in the current channel!",
+	desc: "Sends link to add the discord bot! Not specifying where the link should be sent would mean the bot will send the message in the current channel!",
 	async execute(bot, messageCreate, args, prefix){
 		if(!args[0]) return messageCreate.channel.send("https://jasbot.senghong.xyz")
 		if(args[0] == "dm") return messageCreate.author.send("https://jasbot.senghong.xyz").catch(()=> {
