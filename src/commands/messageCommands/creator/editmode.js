04ea28fb4f98e6c33a364404ca5500444b2ae740
pendragons-code <pendragonscode@gmail.com>
2023-03-05 15:44:55 +0800

    minor fix

--- src/commands/messageCommands/creator/editmode.js
+++ src/commands/messageCommands/creator/editmode.js
@@ -13,7 +13,31 @@ module.exports = {
 		if(editmode === null) editmode = "off"  // Will no longer use "1" and "0" to represent editmode state.
 		switch(args[0]) {
 			case "on":
+				if(editmode === "on") return messageCreate.channel.send("Editmode is already on!")
+				await db.set("editmode", "on").catch((error) => {
+					console.error(error)
+					console.log(messageCreate.content)
+					return messageCreate.channel.send(reject.WeAreScrewed.ExecutionError)
+				})
+				messageCreate.channel.send("Turning on editmode.")
 				break
+
+			case "off":
+				if(editmode === "off") return messageCreate.channel.send("Editmode is already off!")
+				await db.delete("editmode").catch((error) => {
+					console.error(error)
+					console.log(messageCreate.content)
+					return messageCreate.channel.send(reject.WeAreScrewed.ExecutionError)
+				})
+				messageCreate.channel.send("Turning off editmode.")
+				break
+			
+			case "check":
+				messageCreate.channel.send(`Editmode is ${editmode}!`)
+				break
+
+			default:
+				return messageCreate.channel.send(reject.UserFault.args.invalid)
 		}
 	}
 }
