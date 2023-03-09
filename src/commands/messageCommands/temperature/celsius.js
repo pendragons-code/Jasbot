c61c1ce32e7ca481b664d5756697cc557ccf17ef
pendragons-code <pendragonscode@gmail.com>
2023-03-09 21:05:00 +0800

    Prolly a mistake somewhere. That said, I will be getting this guy up soon!

---
+++ src/commands/messageCommands/temperature/celsius.js
@@ -0,0 +1,25 @@
+const { convertCelsius } = require("../../../functions/conversion/temperature/celsius.js")
+const { EmbedBuilder } = require("discord.js")
+const { Default, Bot } = require("../../../../config.json")
+const { conversion } = require("../../../../assets/responseComponents/disclaimers.json")
+const reject = require("../../../../assets/responseComponents/rejection.json")
+module.exports = {
+	name: "celsius",
+	aliases: [],
+	category: "temperature",
+	desc: conversion.value,
+	utilisation: "temperature <value>",
+	async execute(bot, messageCreate, args, mainPrefix) {
+		if(!args[0]) return messageCreate.channel.send(reject.UserFault.numbers.missing)
+		if(isNaN(args[0])) return messageCreate.channel.send(reject.UserFault.numbers.invalid)
+		if(args[1]) return messageCreate.channel.send(reject.UserFault.numbers.tooMany)
+		let convertedEmbed = new EmbedBuilder()
+		convertedEmbed.setTitle("Converting!")
+		convertedEmbed.setFooter({ text: Default.DefaultFooterText })
+		convertedEmbed.setColor(Default.DefaultFooterText)
+		convertedEmbed.setTimestamp()
+		convertedEmbed.setDescription(await convertCelsius(args[0]))
+		convertedEmbed.setURL(Bot.BotSite)
+		return messageCreate.channel.send({ embeds: [convertedEmbed] })
+	}
+}
