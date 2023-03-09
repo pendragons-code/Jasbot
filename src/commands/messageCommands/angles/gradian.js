2cc7d20baacea13f17c61d1c0d417bc0baf2940f
pendragons-code <pendragonscode@gmail.com>
2023-03-09 21:14:02 +0800

    Some angles shit.

--- src/commands/messageCommands/angles/gradian.js
+++ src/commands/messageCommands/angles/gradian.js
@@ -1,14 +1,14 @@
-const { degrees } = require("../../../functions/conversion/angles/degrees.js")
+const { gradian } = require("../../../functions/conversion/angles/gradian.js")
 const { EmbedBuilder } = require("discord.js")
 const { DefaultEmbedColor, DefaultFooterText, Bot } = require("../../../../config.json")
 const { conversion } = require("../../../../assets/responseComponents/disclaimers.json")
 const reject = require("../../../../assets/responseComponents/rejection.json")
 module.exports = {
-	name: "degrees",
-	aliases: ["degree"],
+	name: "gradian",
+	aliases: ["gradians"],
 	category: "angles",
 	desc: conversion.value,
-	utilisation: "degrees <value>",
+	utilisation: "gradian <value>",
 	async execute(bot, messageCreate, args, mainPrefix) {
 		//Before I go on, I want to emphasize that I will not use the api that i already built, especially since redundancy is important.
 		if(!args[0]) return messageCreate.channel.send(reject.UserFault.args.missing)
@@ -20,7 +20,7 @@ module.exports = {
 		embed.setFooter({ text: DefaultFooterText })
 		embed.setTimestamp()
 		embed.setTitle("Conversion!")
-		embed.setDescription(await degrees(number))
+		embed.setDescription(await gradian(number))
 		embed.setURL(Bot.BotSite)
 		return messageCreate.channel.send({ embeds: [embed] })
 	}
