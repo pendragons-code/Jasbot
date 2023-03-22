c592a802455125579b76c7cb042db3fc5b63c888
pendragons-code <pendragonscode@gmail.com>
2023-03-22 22:25:58 +0800

    Banana time!

--- src/commands/slashCommands/core/help.js
+++ src/commands/slashCommands/core/help.js
@@ -22,25 +22,36 @@ module.exports = {
 		}
 	],
 	async execute(bot, interactionCreate) {
+		if(interactionCreate.options._hoistedOptions[1]) return interactionCreate.reply(reject.UserFault.args.tooMany)
 		const primariyTitleWithDecoration = `${Decoration.Title.first} List of commands! ${Decoration.Title.second}`
 		const slashCommander = bot.slashCommands.filter(x => x)
-		const noArgumentHelpEmbed = new EmbedBuilder()
+		const HelpEmbed = new EmbedBuilder()
+		HelpEmbed.setTitle(primariyTitleWithDecoration)
+		HelpEmbed.setColor(Default.DefaultEmbedColor)
+		HelpEmbed.setFooter({ text: Default.DefaultFooterText })
+		HelpEmbed.setTimestamp()
+		HelpEmbed.setURL(Bot.BotSite)
+		HelpEmbed.setDescription(`This bot has ${slashCommander.size} slash commands!`)
 		const categoryArray = []
 		const slashCommanderCategory = bot.slashCommands.map(u => u.category)
 		for(const CategoryName of slashCommanderCategory) {
 			if(!categoryArray.includes(CategoryName)) categoryArray.push(CategoryName)
 		}
+		console.log(categoryArray)
 		if(!interactionCreate.options._hoistedOptions[0]){
-			noArgumentHelpEmbed.setTitle(primariyTitleWithDecoration)
-			noArgumentHelpEmbed.setColor(Default.DefaultEmbedColor)
-			noArgumentHelpEmbed.setFooter({ text: Default.DefaultFooterText })
-			noArgumentHelpEmbed.setTimestamp()
-			noArgumentHelpEmbed.setURL(Bot.BotSite)
-			noArgumentHelpEmbed.setDescription(`This bot has ${slashCommander.size} slash commands!`)
-			noArgumentHelpEmbed.addFields(
+			HelpEmbed.addFields(
 				{ name: "Available categories!", value: "`help <category>`\n\n`" + categoryArray.join("`, `")+ "`", inline: true }
 			)
-			return interactionCreate.reply({ embeds: [noArgumentHelpEmbed] })
+			return interactionCreate.reply({ embeds: [HelpEmbed] })
 		}
+		let actionWord = interactionCreate.options._hoistedOptions[0].value
+		if(categoryArray.includes(actionWord)){
+			let categorySpecificSlashCommands = bot.slashCommands.filter(command => command.category === actionWord)
+			HelpEmbed.addFields(
+				{ name: "Available commands!", value: "`" + categorySpecificSlashCommands.map(cmd => cmd.name).join("`, `") + "`", inline: true }
+			)
+			return interactionCreate.reply({ embeds: [HelpEmbed] })
+		}
+		//command specific help fetch
 	}
 }
