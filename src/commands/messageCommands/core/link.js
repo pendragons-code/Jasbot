f926c330d2f73c00bd72fc6c76792ba1f2616741
pendragons-code <pendragonscode@gmail.com>
2023-04-17 16:05:47 +0800

    Wow

--- src/commands/messageCommands/core/link.js
+++ src/commands/messageCommands/core/link.js
@@ -5,7 +5,7 @@ module.exports = {
 	utilisation: "link <dm/here>",
 	desc: "Sends links to add the discord bot! Not specifying where the link should be sent would mean the bot will send the message in the current channel!",
 	async execute(bot, messageCreate, args, mainPrefix) {
-		if(!args[0] || args[0] ==="here") return messageCreate.channel.send("https://jasbot.pendragonscode.xyz")
+		if(!args[0] || args[0] === "here") return messageCreate.channel.send("https://jasbot.pendragonscode.xyz")
 		if(args[0] == "dm") return messageCreate.author.send("https://jasbot.pendragonscode.xyz").catch((error) => {
 			console.error(error)
 			messageCreate.channel.send("https://jasbot.pendragonscode.xyz")
